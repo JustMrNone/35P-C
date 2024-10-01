1cd7098de99ddd5b473c1af4d2af4e3d5751f2c5
Kamyar Bashar <kamyar.bashar@gmail.com>
2024-10-01 11:14:00 +0330

    recursive version of the factorial added

---
+++ 5/recursive.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+long long factorial(int n);
+int main(void)
+{
+    int n; 
+    scanf_s("%i", &n);
+    if (n < 1)
+    {
+        printf("Invalid input. Please enter a positive integer.\n");
+        return 1;
+    }
+    long long factorialans = factorial(n);
+    printf("%lli", factorialans);
+}
+
+long long factorial(int n)
+{
+    if(n == 1 || n == 1)
+    {
+        return 1;
+    }
+    return n * factorial(n - 1);
+}
